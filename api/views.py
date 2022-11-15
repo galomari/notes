8bd38f9ec84ee0c06223b702f680d1db1f78daf9
gaeth <75580564+galomari@users.noreply.github.com>
2022-11-15 11:02:01 -0600

    first commit

---
+++ api/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from django.http import request
+from .serializers import NoteSerializer
+from .models import Note
+# Create your views here.
+  
+@api_view(['GET'])
+def getNotes(request):
+      notes=Note.objects.all()
+      serializer=NoteSerializer(notes,many=True)
+      return Response(serializer.data)
+    
+@api_view(['GET'])
+def getNote(request,pk):
+      note=Note.objects.get(id=pk)
+      serializer=NoteSerializer(note,many=False)
+      return Response(serializer.data)
+
+@api_view(['POST'])
+def createNote(request):
+      data=request.data
+      
+      note=Note.objects.create(
+        body=data['body']
+      )
+      
+      serializer=NoteSerializer(note,many=False)
+      return Response(serializer.data)
+@api_view(['PUT'])
+def updateNote(request,pk):
+      data=request.data
+      
+      note=Note.objects.get(
+        id=pk
+      )
+      
+      serializer=NoteSerializer(note,data=request.data)
+      if serializer.is_valid():
+            serializer.save()
+      return Response(serializer.data)
+    
+@api_view(['DELETE'])
+def deleteNote(request,pk):
+      note= Note.objects.get(id=pk)
+      note.delete
+      return Response('Note was deleted')
